d5fc555f0d46ff5d8aaa6b14da9fc04028f1bf94
Gary Hockin <gary@roave.com>
2024-06-12 17:36:04 +0100

    Added extra logging

--- app/Http/Controllers/Api/TriggerController.php
+++ app/Http/Controllers/Api/TriggerController.php
@@ -5,12 +5,15 @@ namespace App\Http\Controllers\Api;
 use App\Events\OverlayTriggerEvent;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Log\Logger;
+use Illuminate\Support\Facades\Log;
 
 class TriggerController extends Controller
 {
     public function __invoke(): JsonResponse
     {
         OverlayTriggerEvent::dispatch(['something' => 'happened']);
+        Log::info('Triggering Response');
         return new JsonResponse('OK');
     }
 }
